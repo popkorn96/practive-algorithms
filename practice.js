92657091fb8347e035d02241181101eb70fa3a12
popkorn96 <mpaloma2014@gmail.com>
2021-02-22 14:38:48 -0600

    configuring display for tests

--- practice.js
+++ practice.js
@@ -4,13 +4,26 @@ function expressionCheck(p1, p2) {
     return p1 * p2
 }
 
-function doSomething() {
-    // The console.info()method outputs an informational message to the Web Console. In Firefox, a small 'i' icon is displayed next to these items int he Web Console's log.
-    console.info('DOM loaded')
-}
-
 // This event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
 // If you want the DOM to get parsed as fast as possible after the user has requested the page, you can make your JS asynchronous*
 // *Asynchronous programming is a form of parallel programming that allows a unit of workk to run *seperately* from the primary application thread. 
-document.addEventListener("DOMContentLoaded", doSomething)
+document.addEventListener("DOMContentLoaded", init)
+
+// This variable identifies an HTML element and assigns it variable 'addTwoSum'
+const addTwoSum = document.getElementById('open-first-algorithm')
+
+// This function is inititialized as soon as the DOMContentLoaded has been fired. 
+function init() {
+    // The console.info()method outputs an informational message to the Web Console. In Firefox, a small 'i' icon is displayed next to these items int he Web Console's log.
+    console.info('DOM loaded')
+    // This event listener 
+    addTwoSum.addEventListener('click', displayTwoSum)
+
+}
+
+function displayTwoSum() {
+    var x = getElementById
+}
+
+
 
