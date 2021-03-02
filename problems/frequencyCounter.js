bd7ee17949f0cae318153b0fd30d7667a7dc5814
popkorn96 <mpaloma2014@gmail.com>
2021-03-02 10:44:52 -0600

    adding comments

--- problems/frequencyCounter.js
+++ problems/frequencyCounter.js
@@ -42,3 +42,45 @@ function validAnagram(arr1, arr2){
     
   // add whatever parameters you deem necessary - good luck!
 }
+function sameFrequency(val1, val2){
+    let first = val1.toString();
+    let second = val2.toString();
+    if(first.length !== second.length){
+        return false;
+    }
+    let lookup = {};
+    for(let i = 0; i < first.length; i++){
+        let letter = first[i];
+        lookup[letter] ? lookup[letter] += 1: lookup[letter] = 1;
+    }
+    console.log(lookup)
+    for(let i = 0; i < second.length; i++){
+        let letter = second[i];
+        if(!lookup[letter]){
+            return false;
+        } else {
+            lookup[letter] - 1;
+        }
+    } return true;
+}
+
+// Implement a function called areThereDuplicates which accepts a variable number of arguments,
+// and checks whether there are any duplicates among the arguments passed in.
+// You can solve this using the fequency counter or the multipe pointers pattern.
+
+// This function was implemented using the multiple pointers pattern
+
+function areThereDuplicates(...args){
+    args.sort((a,b) => a > b);
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+        if(args[start] === args[next]){
+            return true
+        } else{
+            start++
+            next++;
+        }
+    } return false
+}
+
