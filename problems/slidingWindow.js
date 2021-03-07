3cfaafe07ed1071cf1b618d63b8c022b37cb3e78
popkorn96 <mpaloma2014@gmail.com>
2021-03-07 11:04:11 -0600

    practice algos

--- problems/slidingWindow.js
+++ problems/slidingWindow.js
@@ -16,6 +16,3 @@
 // Depending on a certain condition, the window either increases or closes (and a new window is created)
 // Very useful for keeping track of a subset of data in an array/string
 
-// Divide and Conquer (used a lot in binary search)
-// THis pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
-// This pattern can tremendously decrease time complexity
