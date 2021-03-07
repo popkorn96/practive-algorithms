f93b3f1b16857e95a4fae66e272785a6dd4d4e18
popkorn96 <mpaloma2014@gmail.com>
2021-03-07 11:04:25 -0600

    practice algos

--- problems/slidingWindow.js
+++ problems/slidingWindow.js
@@ -13,6 +13,3 @@
 // This pattern involves createing a whindow which can either be an arrau
 // or number from one position to another
 
-// Depending on a certain condition, the window either increases or closes (and a new window is created)
-// Very useful for keeping track of a subset of data in an array/string
-
