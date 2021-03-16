0828807893b20e9743d73ee979626c3bd755f6df
popkorn96 <mpaloma2014@gmail.com>
2021-03-16 12:43:08 -0500

    practice algos

--- problems/slidingWindow.js
+++ problems/slidingWindow.js
@@ -8,3 +8,14 @@
 // Divide and Conquer (used a lot in binary search)
 // THis pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
 // This pattern can tremendously decrease time complexity
+
+// Sliding Window:
+// This pattern involves createing a whindow which can either be an arrau
+// or number from one position to another
+
+// Depending on a certain condition, the window either increases or closes (and a new window is created)
+// Very useful for keeping track of a subset of data in an array/string
+
+// Divide and Conquer (used a lot in binary search)
+// THis pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
+// This pattern can tremendously decrease time complexity
